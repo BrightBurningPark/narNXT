b1bd0a00bd09c16818ef72784a58772a31573e7b
BrightBurningPark <psd9707@gmail.com>
2019-05-22 05:26:56 +0900

    initial commit, and dev done with the nxtOSEK part on capstone

---
+++ USBLoopBack.h
@@ -0,0 +1,14 @@
+#ifndef _USBLOOPBACK_H
+#define _USBLOOPBACK_H
+
+#define DISCONNECT_REQ 0xFF
+#define COMM_STRING 0x01
+#define ACK_STRING 0x02
+
+#define STOP "0"
+#define FORWARD "1"
+#define BACKWARD "2"
+#define LEFT "3"
+#define RIGHT "4"
+
+#endif
